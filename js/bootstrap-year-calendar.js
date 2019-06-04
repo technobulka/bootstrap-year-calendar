181d1c4b91e0a59a4e400feddf01c587f74505e1
Stanislav <technobulka@gmail.com>
2019-06-04 14:36:45 +0300

    fix _isMobile regex

--- js/bootstrap-year-calendar.js
+++ js/bootstrap-year-calendar.js
@@ -872,8 +872,7 @@
 			return false;
 		},
 		_isMobile: function() {
-			var mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk/i;
-			return mobileRE.test(navigator.userAgent);
+			return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent);
 		},
 		getWeekNumber: function(date) {
 			var tempDate = new Date(date.getTime());
